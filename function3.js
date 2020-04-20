fc0c4508ba6ca1d28e9fb9463bc12a52f6010765
Fabricio Alvarado Esquivel <fabriales14@gmail.com>
2020-04-20 13:08:31 -0600

    master: files changed

--- function3.js
+++ function3.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('Hello from Lambda 3!'),
     };
     return response;
 };
