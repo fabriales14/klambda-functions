7d2c84887445d4e906339b1b2f81bdf9732855fe
Fabricio Alvarado <fabriales14@gmail.com>
2020-05-05 11:01:47 -0600

    Update function2.js

--- function2.js
+++ function2.js
@@ -1,8 +1,7 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda 2!'),
-    };
-    return response;
+exports.handler = (event, context, callback) => {    
+    // Assign the support case and update the status message    
+    var myCaseID = event.Case;    
+    var myMessage = event.Message + "assigned...";    
+    var result = {Case: myCaseID, Message: myMessage};
+    callback(null, result);        
 };
