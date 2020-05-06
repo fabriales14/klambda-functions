530cb6824da5e0cb53fe2f1fc8d05df179538f75
Fabricio Alvarado <fabriales14@gmail.com>
2020-05-06 16:30:01 -0600

    Update function1.js

--- function1.js
+++ function1.js
@@ -1,7 +1,24 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: "database-1.cb3lrcwcl2pb.us-west-1.rds.amazonaws.com",
+    user: "admin",
+    password: "12345678",
+    database: "database-1",
+});
+// console.log(connection);
 exports.handler = (event, context, callback) => {
-    // Create a support case using the input as the case ID, then return a confirmation message   
-   var myCaseID = event.inputCaseID;
-   var myMessage = "Case " + myCaseID + ": opened...";   
-   var result = {Case: myCaseID, Message: myMessage};
-   callback(null, result);    
+    connection.query('show tables', function (error, results, fields) {
+        if (error) {
+            connection.destroy();
+            throw error;
+        } else {
+            // connected!
+            console.log(results);
+            connection.end(function (err) { callback(err, results);});
+        }
+    });
+    var myCaseID = event.inputCaseID;
+    var myMessage = "Case " + myCaseID + ": opened...";   
+    var result = {Case: myCaseID, Message: myMessage};
+    callback(null, result); 
 };
