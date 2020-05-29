d8f857258a7a3ad96eff1fb1385b610032c60233
Fabricio Alvarado Esquivel <fabriales14@gmail.com>
2020-05-29 16:46:57 -0600

    changes in files

--- function2.js
+++ function2.js
@@ -1,7 +1,8 @@
-exports.handler = (event, context, callback) => {    
-    // Assign the support case and update the status message    
+exports.handler = (event, context, callback) => { 
+    // Close the support case       
     var myCaseID = event.Case;    
-    var myMessage = event.Message + "assigned...";    
+    var myMessage = event.Message + " readed... ";    
     var result = {Case: myCaseID, Message: myMessage};
-    callback(null, result);        
+    callback(null, result);
 };
+
