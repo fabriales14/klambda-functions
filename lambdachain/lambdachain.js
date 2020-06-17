5b6bf9bec015f5e8f78f7c960c56a72dd69ab563
Fabricio Alvarado Esquivel <fabriales14@gmail.com>
2020-06-17 11:18:01 -0600

    changes in chain

--- lambdachain/lambdachain.js
+++ lambdachain/lambdachain.js
@@ -1,26 +1,24 @@
-var AWS = require('aws-sdk');
+var AWS = require('aws-sdk'); // sdk de aws para invocación de Lambdas
 var lambda = new AWS.Lambda({
-    region: process.env.region
+    region: process.env.region // configuración de región
 });
-var lambdachain = process.env.listLambdas.split(' ');
-var output;
+var lambdachain = process.env.listLambdas.split(' '); // obtención de lista de funciones a invocar
+var output; // datos para retornar al final del proceso
 
 exports.handler = function(event, context, callback) {
-    console.log(event);
-    const main = async () => {
+    const main = async () => { // invocación asíncrona de primera función
         output = await (lambda.invoke({
             FunctionName: lambdachain[0], 
             Payload: JSON.stringify(event),
         }).promise());
-    
-        for (var x=1; x<lambdachain.length; x++){
-            output = await (lambda.invoke({
+        
+        for (var x=1; x<lambdachain.length; x++){ // ciclo de ejecución
+            output = await (lambda.invoke({ // input es el output de última llamada
                 FunctionName: lambdachain[x], 
                 Payload: output.Payload,
             }).promise());
         }
-        console.log(output);
     };
-    main().catch(error => console.error(error));
-    callback(null, output);
+    main().catch(error => console.error(error)); // retorno en caso de error
+    callback(null, output); // devuelve el resultado construído
 };
