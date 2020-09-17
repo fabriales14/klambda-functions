ddd8a5f005d9e551ed383c1c984c7761fec72100
Fabricio Alvarado Esquivel <fabriales14@gmail.com>
2020-09-17 14:38:15 -0600

    files updated

--- print/print.js
+++ print/print.js
@@ -2,6 +2,7 @@ exports.handler =  async function(event, context) {
     const response = {
         statusCode: 200,
         message: event.body.message,
+        input: event
     };
     return response
 }
